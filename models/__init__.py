73af522656d32624270d0d471ab8040f0acc6854
Sintayehu Mulugeta Kebede <sintayehumulugeta77@gmail.com>
2024-08-18 20:24:09 +0300

    Sartup part of the project

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-"""Initialize and packagize the models directory/folder"""
-from models.engine.file_storage import FileStorage
+""" Initialize and packagize the models directory/folder
 
+    Creates a link between the application and the Data Storage
+    of interest i.e either FileStorage or DBStorage
+"""
+from os import getenv
 
-storage = FileStorage()
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:  # getenv('HBNB_TYPE_STORAGE') is 'FileStorage':
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 storage.reload()
