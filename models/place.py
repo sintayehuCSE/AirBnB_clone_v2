0156175474d6c1f26d2bc7f38a72a00949412fa3
Sintayehu Mulugeta Kebede <sintayehumulugeta77@gmail.com>
2024-08-20 16:54:59 +0000

    Place Model

--- models/place.py
+++ models/place.py
@@ -1,19 +1,37 @@
-#!/usr/bin/python3
+B11;rgb:0000/0000/0000#!/usr/bin/python3
 """The place module: It Holds the Place class of the Project."""
-from models.base_model import BaseModel
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
+from models.base_model import BaseModel, Base
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """Define the Place class."""
+    __tablename__ = 'places'
 
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
+        number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
+        price_by_night = Column(Integer, nullable=False, default=0)
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
+        user = relationship('User', back_populates='places')
+        cities = relationship('City', back_populates='places')
+    else:  # If Storage env is FileStorage
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
