0ff937bd4188d174463abf66adad09f78e6d4e21
Sintayehu Mulugeta Kebede <sintayehumulugeta77@gmail.com>
2024-08-21 14:21:31 +0000

    Amenity Model

--- models/amenity.py
+++ models/amenity.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
 """The amenity module: Holds Amenity class of the project."""
-from models.base_model import BaseModel
+from sqlalchemy import Column, String, Table, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
+from models.base_model import BaseModel, Base
+from models.place import place_amenity
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """Defines the amenity class."""
-    name = ""
+    __tablename__ = 'amenities'
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        places = relationship('Place', secondary=place_amenity, back_populates='amenities')
+    else:  # If Storage env is FileStorage
+        name = ""
