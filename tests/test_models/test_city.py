e587f1e243db8dd2e43880967617f58ec152db34
Sintayehu Mulugeta Kebede <sintayehumulugeta77@gmail.com>
2024-08-22 20:44:52 +0300

    Test the City

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Test the city module."""
 import unittest
+from tests.test_models.test_base_model import test_basemodel
 from models.city import City
 
 class Test_City(unittest.TestCase):
@@ -18,6 +19,25 @@ class Test_City(unittest.TestCase):
     def test_city_state_id(self):
         """Test the state Id in City class."""
         self.assertIs(str, type(City.state_id))
+
+class test_City(test_basemodel):
+    """ """
+
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = "City"
+        self.value = City
+
+    def test_state_id(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.state_id), str)
+
+    def test_name(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.name), str)
 
 
 if __name__ == "__main__":
