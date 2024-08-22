31f91041c3e0fdd5e670c9c4f7e8928b1c70b909
Sintayehu Mulugeta Kebede <sintayehumulugeta77@gmail.com>
2024-08-22 20:46:45 +0300

    Test the Place

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Test the place module."""
 import unittest
+from tests.test_models.test_base_model import test_basemodel
 from models.place import Place
 
 class Test_Amenity(unittest.TestCase):
@@ -54,7 +55,70 @@ class Test_Amenity(unittest.TestCase):
     def test_place_amenity_id(self):
         """Test the type of place amenity_id."""
         self.assertIs(list, type(Place.amenity_ids))
-    
+ 
+class test_Place(test_basemodel):
+    """ """
+
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = "Place"
+        self.value = Place
+
+    def test_city_id(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.city_id), str)
+
+    def test_user_id(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.user_id), str)
+
+    def test_name(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.name), str)
+
+    def test_description(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.description), str)
+
+    def test_number_rooms(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.number_rooms), int)
+
+    def test_number_bathrooms(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.number_bathrooms), int)
+
+    def test_max_guest(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.max_guest), int)
+
+    def test_price_by_night(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.price_by_night), int)
+
+    def test_latitude(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.latitude), float)
+
+    def test_longitude(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.latitude), float)
+
+    def test_amenity_ids(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.amenity_ids), list)
 
 if __name__ == "__main__":
     unittest.main()
