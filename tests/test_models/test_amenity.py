e4b07de4bd7fdc20fb025284e68ae8655a3b4fa9
Sintayehu Mulugeta Kebede <sintayehumulugeta77@gmail.com>
2024-08-22 20:43:43 +0300

    Test the Amenity

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Test the amenity module."""
 import unittest
+from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
 
 class Test_Amenity(unittest.TestCase):
@@ -14,7 +15,20 @@ class Test_Amenity(unittest.TestCase):
 	def test_amenity_name(self):
 		"""Check the type of Amenity name."""
 		self.assertIs(str, type(Amenity.name))
-	
+
+class test_Amenity(test_basemodel):
+    """ """
+
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = "Amenity"
+        self.value = Amenity
+
+    def test_name2(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.name), str)
 
 if __name__ == "__main__":
 	unittest.main()
