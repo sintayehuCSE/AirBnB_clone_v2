cbf020805b55b9e3051fc42b6fd49d6c31f21ac6
Sintayehu Mulugeta Kebede <sintayehumulugeta77@gmail.com>
2024-08-22 20:47:38 +0300

    Test the State

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Test the state module."""
+from tests.test_models.test_base_model import test_basemodel
 import unittest
 from models.state import State
 
@@ -14,6 +15,20 @@ class Test_State(unittest.TestCase):
     def test_state_name(self):
         """Check the type of state name."""
         self.assertIs(str, type(State.name))
+
+class test_state(test_basemodel):
+    """ """
+
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = "State"
+        self.value = State
+
+    def test_name3(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.name), str)
 
 
 if __name__ == "__main__":
