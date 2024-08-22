3c85da448b06a0856a0d05e63de67dcc3608131d
Sintayehu Mulugeta Kebede <sintayehumulugeta77@gmail.com>
2024-08-22 15:43:57 +0300

    The Initializer

--- models/__init__.py
+++ models/__init__.py
@@ -5,6 +5,13 @@
     of interest i.e either FileStorage or DBStorage
 """
 from os import getenv
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 if getenv('HBNB_TYPE_STORAGE') == 'db':
     from models.engine.db_storage import DBStorage
