80f328955c9d8fb89611315f3237c95b5c30ff9a
Sintayehu Mulugeta Kebede <sintayehumulugeta77@gmail.com>
2024-08-22 20:47:14 +0300

    Test the Review

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Test the city module."""
 import unittest
+from tests.test_models.test_base_model import test_basemodel
 from models.review import Review
 
 class Test_Review(unittest.TestCase):
@@ -23,6 +24,29 @@ class Test_Review(unittest.TestCase):
         """Test the type of revie text."""
         self.assertIs(str, type(Review.text))
 
+class test_review(test_basemodel):
+    """ """
+
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = "Review"
+        self.value = Review
+
+    def test_place_id(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.place_id), str)
+
+    def test_user_id(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.user_id), str)
+
+    def test_text(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.text), str)
 
 if __name__ == "__main__":
     unittest.main()
