bdc9b8cbcd41d4386d0cf9bb14e308aa0b60622d
Sintayehu Mulugeta Kebede <sintayehumulugeta77@gmail.com>
2024-08-22 11:50:50 +0300

    Amenity Model

--- models/amenity.py
+++ models/amenity.py
@@ -13,6 +13,7 @@ class Amenity(BaseModel, Base):
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
-        places = relationship('Place', secondary=place_amenity, back_populates='amenities')
+        places = relationship('Place', secondary=place_amenity,
+                              back_populates='amenities')
     else:  # If Storage env is FileStorage
         name = ""
