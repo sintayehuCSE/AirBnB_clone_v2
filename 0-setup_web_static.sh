0edad48362d70d7dd3c7e86753a8036a97d69fde
Sintayehu Mulugeta Kebede <sintayehumulugeta77@gmail.com>
2024-09-11 01:02:00 +0300

    Web Static Deplyment Script

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 # A Bash script that sets up your web servers for the deployment of web_static
 
+sudo apt-get -y update
+sudo apt-get -y install nginx
+
 sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
-printf % "
-!DOCTYPE html
+printf %s "<!DOCTYPE html>
 <html>
 	<head>
 		<title>Holberton Deploy Static</title>
@@ -15,13 +17,12 @@ printf % "
 		<p>Holberton School</p>
 	</body?
 </html>
-" > /data/web_static/releases/test/index.html
+" | sudo tee /data/web_static/releases/test/index.html
 
 sudo ln --symbolic --force /data/web_static/releases/test/ /data/web_static/current
 sudo chown --recursive -L ubuntu:ubuntu /data/
 
-new_location="server_name _;\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}"
-replace="server_name _;"
-sudo sed -i "s/$replace/$new_location/" /etc/nginx/sites-available/default
+new_location="server_name _;\n\tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/;\n\t}"
+sudo sed -i "s/server_name _;/$new_location/" /etc/nginx/sites-available/default
 
 sudo service nginx restart
