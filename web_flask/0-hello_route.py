dad8798b0a372770cc399a2cd4b397b7c67bd726
Sintayehu Mulugeta Kebede <sintayehumulugeta77@gmail.com>
2024-09-23 12:18:15 +0300

    Start Web Application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""An interface to Flask Web Application"""
+from flask import Flask
+
+
+app = Flask(__name__)
+app.config['SERVER_NAME'] = "0.0.0.0:5000"
+
+@app.route('/', strict_slashes=False)
+def index():
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run()
