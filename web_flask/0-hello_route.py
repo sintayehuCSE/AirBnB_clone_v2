3d4845cacc14f78fbb67ca0756df1d2b61565afd
Sintayehu Mulugeta Kebede <sintayehumulugeta77@gmail.com>
2024-09-23 14:30:56 +0300

    Start Web Application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,9 +6,11 @@ from flask import Flask
 app = Flask(__name__)
 app.config['SERVER_NAME'] = "0.0.0.0:5000"
 
+
 @app.route('/', strict_slashes=False)
 def index():
     return "Hello HBNB!"
+
 
 if __name__ == '__main__':
     app.run()
